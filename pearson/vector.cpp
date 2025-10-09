676657713a57fe31d51b78a588aebfea8d7c975c
ante237 <anton.helmersson00@gmail.com>
2025-10-09 14:12:09 +0200

    Added basic simd to Vector

--- pearson/vector.cpp
+++ pearson/vector.cpp
@@ -6,6 +6,7 @@ Author: David Holmqvist <daae19@student.bth.se>
 #include <iostream>
 #include <cmath>
 #include <vector>
+#include <immintrin.h>
 
 Vector::Vector()
     : size{0}, data{nullptr}
@@ -64,11 +65,15 @@ double &Vector::operator[](unsigned i)
 double Vector::mean() const
 {
     double sum{0};
-
-    for (auto i{0}; i < size; i++)
+    __m256d sumVec = _mm256_setzero_pd();
+    for (auto i{0}; i + 3 < size; i += 4)
     {
-        sum += data[i];
+        __m256d chunk = _mm256_loadu_pd(&data[i]);
+        sumVec = _mm256_add_pd(sumVec, chunk);
     }
+    alignas(32) double tmp[4];
+    _mm256_store_pd(tmp, sumVec);
+    sum = tmp[0] + tmp[1] + tmp[2] + tmp[3];
 
     return sum / static_cast<double>(size);
 }
@@ -94,10 +99,13 @@ Vector Vector::operator/(double div)
 Vector Vector::operator-(double sub)
 {
     auto result{*this};
+    __m256d scalar = _mm256_set1_pd(sub);
 
-    for (auto i{0}; i < size; i++)
+    for (auto i{0}; i + 3 < size; i += 4)
     {
-        result[i] -= sub;
+        __m256d vecSeg = _mm256_loadu_pd(&(result.data)[i]);
+        vecSeg = _mm256_sub_pd(vecSeg, scalar);
+        _mm256_storeu_pd(&(result.data)[i], vecSeg);
     }
 
     return result;
